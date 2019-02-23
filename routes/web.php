b5224fde0797b8d9c3c615fbf36d84a676fe7bc4
Ogi G Permana <hunterdumay@gmail.com>
2019-02-23 16:02:13 +0700

    Add for all feature

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ $router->get('/', function () use ($router) {
 });
 
 $router->group(['prefix' => 'api/v1'], function () use ($router) {
+    // Route for users
     $router->get('users',  ['uses' => 'Actor\UserController@showAllUsers']);
   
     $router->get('users/{id}', ['uses' => 'Actor\UserController@showOneUser']);
@@ -25,4 +26,38 @@ $router->group(['prefix' => 'api/v1'], function () use ($router) {
     $router->delete('users/{id}', ['uses' => 'Actor\UserController@delete']);
   
     $router->put('users/{id}', ['uses' => 'Actor\UserController@update']);
+
+    // Route for Items
+    $router->get('items',  ['uses' => 'Kw\ItemController@showAllItems']);
+  
+    $router->get('items/{id}', ['uses' => 'Kw\ItemController@showOneItem']);
+  
+    $router->post('items', ['uses' => 'Kw\ItemController@create']);
+  
+    $router->delete('items/{id}', ['uses' => 'Kw\ItemController@delete']);
+  
+    $router->put('items/{id}', ['uses' => 'Kw\ItemController@update']);
+
+    // Route for Checklists
+    $router->get('checklists',  ['uses' => 'Kw\ChecklistController@showAllChecklists']);
+  
+    $router->get('checklists/{id}', ['uses' => 'Kw\ChecklistController@showOneChecklist']);
+  
+    $router->post('checklists', ['uses' => 'Kw\ChecklistController@create']);
+  
+    $router->delete('checklists/{id}', ['uses' => 'Kw\ChecklistController@delete']);
+  
+    $router->put('checklists/{id}', ['uses' => 'Kw\ChecklistController@update']);
+
+    // Route for Template
+    $router->get('templates',  ['uses' => 'Kw\TemplateController@showAllTemplates']);
+
+    $router->get('templates/{id}',  ['uses' => 'Kw\TemplateController@showSingleTemplate']);
+
+    $router->post('templates',  ['uses' => 'Kw\TemplateController@create']);
+    
+    $router->put('templates/{id}', ['uses' => 'Kw\TemplateController@update']);
+
+    $router->put('templates/{id}', ['uses' => 'Kw\TemplateController@delete']);
+
   });
